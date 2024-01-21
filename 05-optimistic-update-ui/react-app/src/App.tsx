ea021baa7ddb651c043f6f3b37fe17c235d6e1bb
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-01-22 00:27:01 +0900

    feat : React Query 초기 설정

--- 05-optimistic-update-ui/react-app/src/App.tsx
+++ 05-optimistic-update-ui/react-app/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-function App() {
+import Board from "./Board";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Board />} />
+        </Routes>
+      </Suspense>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </Router>
   );
-}
+};
 
 export default App;
