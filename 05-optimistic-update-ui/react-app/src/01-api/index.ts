30bfd9bf677ebbf80ff8002aa5ea635c0fbc58ae
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-01-22 12:20:22 +0900

    hotfix : webpack 5 polyfill override 수정

--- 05-optimistic-update-ui/react-app/src/01-api/index.ts
+++ 05-optimistic-update-ui/react-app/src/01-api/index.ts
@@ -1,8 +1,6 @@
-import dotenv from 'dotenv';
 import type { Board } from '../00-domain';
-dotenv.config();
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const fetchPosts = async() => {
     const response = await fetch(`${BASE_URL}/api/posts`, {
